0e6b96915fb6348a89719f2e28b4cd21559862b2
Alp3n <adam@walukiewicz.eu>
2020-11-24 20:49:31 +0700

    added collection and collection-item

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Homepage from './views/homepage/homepage.component';
+import ShopPage from './views/shop/shoppage.component';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={Homepage} />
+        <Route exact path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
